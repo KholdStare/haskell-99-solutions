9be28342b29c79877fe1a42b5400dff417267ad0
Alexander Kondratskiy <kholdstare0.0@gmail.com>
2013-04-11 08:20:58 -0400

    Problems 32 to 35 solved

--- test.hs
+++ test.hs
@@ -276,3 +276,55 @@ isPrime = DL.and . predicates
     where predicates x = map (not . isDivisableBy x) [2..bound x]
           isDivisableBy x d = x `mod` d == 0
           bound = fromEnum . sqrt . toEnum
+
+{-problem 32-}
+myGCD :: Int -> Int -> Int
+myGCD a 0 = a
+myGCD a b = myGCD b $ a `mod` b
+
+{-problem 33-}
+coprime :: Int -> Int -> Bool
+coprime a b = gcd a b == 1
+
+{-problem 34-}
+totient :: Int -> Int
+totient 1 = 1
+totient x = DL.foldl' f 1 [2..x-1]
+    where f acc y = if coprime x y
+                    then acc+1
+                    else acc
+
+{-problem 35-}
+
+-- first define prime numbers
+
+doesntDivide :: Int -> Int -> Bool
+doesntDivide x by = x `mod` by > 0
+
+sqrt' :: Int -> Int
+sqrt' = floor.sqrt . fromIntegral
+
+upTo :: [Int] -> Int -> [Int]
+upTo list limit = takeWhile ( (>=) limit ) list
+
+primes :: [Int]
+primes = 2 : [ x | x <- oddNumbers, isPrime x]
+    where oddNumbers = [3,5..]
+          isPrime x = all (doesntDivide x) $ primes `upTo` sqrt' x
+
+-- now define prime factors
+
+primeFactors :: Int -> [Int]
+primeFactors = primeFactors' primes
+
+divides :: Int -> Int -> Bool
+divides a b = b `mod` a == 0
+
+primeFactors' :: [Int] -> Int -> [Int]
+primeFactors' _ 1 = []
+primeFactors' primes@(curPrime:largerPrimes) x =
+            if curPrime `divides` x
+            then curPrime : primeFactors' primes (x `quot` curPrime)
+            else primeFactors' largerPrimes x
+
+
