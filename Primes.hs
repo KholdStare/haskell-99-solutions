2bb92a92538a115f26f63f85b8f46dc961df2ebd
Alexander Kondratskiy <kholdstare0.0@gmail.com>
2013-09-04 17:21:24 -0400

    Separating "." of composition

--- Primes.hs
+++ Primes.hs
@@ -5,7 +5,7 @@ doesntDivide :: Int -> Int -> Bool
 doesntDivide x by = x `mod` by > 0
 
 sqrt' :: Int -> Int
-sqrt' = floor.sqrt . fromIntegral
+sqrt' = floor . sqrt . fromIntegral
 
 upTo :: [Int] -> Int -> [Int]
 upTo list limit = takeWhile ( (>=) limit ) list
